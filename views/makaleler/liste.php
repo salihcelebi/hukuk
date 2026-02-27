25c840f944027ccb04fcd01cf5f787add9e26fed
salihcelebi <92729599+salihcelebi@users.noreply.github.com>
2026-02-27 14:26:18 -0800

    Update articles list view to English UI per Arayuz talimati

--- views/makaleler/liste.php
+++ views/makaleler/liste.php
@@ -1,31 +1,32 @@
 <!DOCTYPE html>
-<html lang="tr">
+<html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Makale Listesi</title>
+    <title>Browse Articles</title>
 </head>
 <body>
-    <h1>Makale Listesi</h1>
-    <p>Bu sayfa, tüm makaleleri listeler. Makale başlığına tıkladığınızda makale detay sayfasına gidersiniz.</p>
-    <?php
-    // $makaleler değişkeni MakaleDenetleyici'nin liste() metodunda doldurulur.
-    // Her makale için 'baslik', 'slug' ve isteğe bağlı olarak 'ozet' alanları bulunur.
-    ?>
-    <?php if (!empty($makaleler)): ?>
-        <ul>
-        <?php foreach ($makaleler as $makale): ?>
-            <li>
-                <a href="/makaleler/<?php echo htmlspecialchars($makale['slug'], ENT_QUOTES, 'UTF-8'); ?>">
-                    <?php echo htmlspecialchars($makale['baslik'], ENT_QUOTES, 'UTF-8'); ?>
-                </a>
-                <?php if (isset($makale['ozet']) && $makale['ozet'] !== ''): ?>
-                    <p><?php echo htmlspecialchars($makale['ozet'], ENT_QUOTES, 'UTF-8'); ?></p>
-                <?php endif; ?>
-            </li>
-        <?php endforeach; ?>
-        </ul>
-    <?php else: ?>
-        <p>Henüz makale bulunamadı.</p>
-    <?php endif; ?>
+<h1>Browse Articles</h1>
+<p>This page lists all articles. Click an article title to view details.</p>
+<?php
+// $makaleler degiskeni MakaleDenetleyici->liste() metodundan gelir.
+// Her makale icin 'baslik', 'slug' ve istege bagli olarak 'ozet' alanlari bulunur.
+// Arayuz Talimati: Public UI Ingilizce olmalidir [434203738162501]
+?>
+<?php if (!empty($makaleler)): ?>
+    <ul>
+    <?php foreach ($makaleler as $makale): ?>
+        <li>
+            <a href="/makaleler/<?php echo htmlspecialchars($makale['slug'], ENT_QUOTES, 'UTF-8'); ?>">
+                <?php echo htmlspecialchars($makale['baslik'], ENT_QUOTES, 'UTF-8'); ?>
+            </a>
+            <?php if (isset($makale['ozet']) && $makale['ozet'] != ''): ?>
+                <p><?php echo htmlspecialchars($makale['ozet'], ENT_QUOTES, 'UTF-8'); ?></p>
+            <?php endif; ?>
+        </li>
+    <?php endforeach; ?>
+    </ul>
+<?php else: ?>
+    <p>No articles found.</p>
+<?php endif; ?>
 </body>
 </html>
